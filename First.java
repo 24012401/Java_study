171c3b669df8a84b6cd75daf325f0b61b36da137
24012401 <2697083380qq.com>
2020-07-04 19:17:35 +0800

    Solve the first question.

---
+++ First.java
@@ -0,0 +1,9 @@
+// 题目：输出长春市地铁1号线的运行路线
+
+public class First {
+	public static void main(String[] args) {
+        System.out.println("北环城战   一匡街    胜利公园    解放大路    工农广场    卫星广场    华庆路");
+        System.out.println("|············|·········|···········|···········|···········|·········|······");
+        System.out.println("   庆丰路    长春北站   人民广场    东北师大       繁荣路      市政府    红嘴子");
+	}
+}
