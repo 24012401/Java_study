00694e83ec8da9c98def4116b572f8d43a859b5f
24012401 <2697083380@qq.com>
2020-07-04 19:54:04 +0800

    Solve the second question.

--- Second.java
+++ Second.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+
 public class Second {
-    
+    public static void main(String[] args) {
+        char ch;
+        Scanner in = new Scanner(System.in);
+        System.out.print("输文件中的字符：");
+        ch = in.next().charAt(0);
+        System.out.println(ch + "替换为：" + (int)(ch + 0));
+    }
 }
