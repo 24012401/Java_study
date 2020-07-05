dcadf5d021ac18340ed2eed2d370272bc8970a44
24012401 <2697083380@qq.com>
2020-07-05 19:45:57 +0800

    Solve the fifth question.

---
+++ 从入门到精通课后实战/Fifth.java
@@ -0,0 +1,11 @@
+//输出歌词“我的未来不是梦”
+
+package 从入门到精通课后实战;
+
+public class Fifth {
+    public static void main(String[] args) {
+        System.out.print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&" + '\n'
+                        +"&       我的未来不是梦         &" + '\n'
+                        +"&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&" + '\n');
+    }
+}
