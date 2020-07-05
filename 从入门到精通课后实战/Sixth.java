1fa8c402e3224f79a66b1ca7d68c568ef75742aa
24012401 <2697083380@qq.com>
2020-07-05 19:53:22 +0800

    Solve the sixth question.

---
+++ 从入门到精通课后实战/Sixth.java
@@ -0,0 +1,14 @@
+//输出明日学院欢迎信息及网址
+
+package 从入门到精通课后实战;
+
+public class Sixth {
+    public static void main(String[] args) {
+        System.out.print("+-------------------------------+" + '\n'
+                        +"|      欢迎登陆明日学院网站     |" + '\n'
+                        +"|                               |" + '\n'
+                        +"|      www.mingrisoft.com       |" + '\n'
+                        +"|                               |" + '\n'
+                        +"+-------------------------------+" + '\n');
+    }
+}
