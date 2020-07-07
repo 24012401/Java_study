f6169b5f43f78dd2feebfd758d9c68f3fe0aec95
24012401 <2697083380@qq.com>
2020-07-07 21:32:55 +0800

    Solved the seventh and eighth qusetions.

---
+++ 从入门到精通课后实战/Eighth.java
@@ -0,0 +1,16 @@
+// 输出对联
+
+package 从入门到精通课后实战;
+
+public class Eighth {
+    public static void main(String[] args) {
+        System.out.print("     福星高照     " + '\n'
+                        +"万              一" + '\n'
+                        +"事              帆" + '\n'
+                        +"如              风" + '\n'
+                        +"意              顺" + '\n'
+                        +"步              年" + '\n'
+                        +"步              年" + '\n'
+                        +"高              好" + '\n');
+    }
+}
---
+++ 从入门到精通课后实战/Seventh.java
@@ -0,0 +1,16 @@
+// 输出世界上最好的六个医生
+
+package 从入门到精通课后实战;
+
+public class Seventh {
+    public static void main(String[] args) {
+        System.out.print("~~~~~~~" + '\n'
+                        +"*1.阳光" + '\n'
+                        +"*2.休息" + '\n'
+                        +"*3.锻炼" + '\n'
+                        +"*4.饮食" + '\n'
+                        +"*5.自信" + '\n'
+                        +"*6.朋友" + '\n'
+                        +"--------");
+    }
+}
